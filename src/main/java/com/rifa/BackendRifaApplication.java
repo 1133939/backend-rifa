6c7239d9aa46917a8561c16aa320040c86f9485f
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-04-02 00:14:19 -0300

    projeto criado

---
+++ src/main/java/com/rifa/BackendRifaApplication.java
@@ -0,0 +1,13 @@
+package com.rifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRifaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRifaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rifa/BackendRifaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rifa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendRifaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
